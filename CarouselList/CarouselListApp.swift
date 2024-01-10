f0152f7c11a4dc7c017587f718e59d95a4e5aa68
shenxiang <shenxiang@foursmile001.com>
2024-01-10 15:16:18 +0800

    Initial Commit

---
+++ CarouselList/CarouselListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselListApp.swift
+//  CarouselList
+//
+//  Created by FS on 2024/1/10.
+//
+
+import SwiftUI
+
+@main
+struct CarouselListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
